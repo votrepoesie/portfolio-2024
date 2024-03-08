8dccbf39a22f22f1e70b2b520941679b545722b7
Serina Khanna <57045574+helloserina@users.noreply.github.com>
2024-03-08 14:25:55 -0500

    trying to make design go to design page

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ class SpecialHeader extends HTMLElement {
                 
                     <ul class="nav-links">
                         <li class="nav-item draw"><a href="html/draw.html">I draw,</a></li>
-                        <li class="nav-item design"><a href="html/design.html">design,</a></li>
+                        <li class="nav-item design"><a href="~/html/design.html">design,</a></li>
                         <li class="nav-item code"><a href="html/code.html">code,</a></li>
                         <li class="nav-item make"><a href="html/make-things.html">and make things.</a></li>
                     </ul>
